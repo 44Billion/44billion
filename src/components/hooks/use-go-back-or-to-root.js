9ad633c1548074cb0f5fde9a9ec42c0c419ed3fa
arthurfranca <arthur.a.franca@gmail.com>
2025-12-09 16:00:29 -0300

    Prune stale path branch while correctly updating route distance

--- src/components/hooks/use-go-back-or-to-root.js
+++ src/components/hooks/use-go-back-or-to-root.js
@@ -4,11 +4,14 @@ import useLocation from '#hooks/use-location.js'
 export default function useGoBackOrToRoot () {
   const loc = useLocation()
   return useCallback(() => {
-    const isAtFirstUrl = !loc.uidCounter$()
+    const currentUid = loc.route$().uid
+    const isAtFirstUrl = currentUid <= 0
 
     if (isAtFirstUrl) {
-      const isAtRoot = loc.route$().url.pathname === '/' && loc.route$().url.searchParams.size === 0
+      const isAtRoot = loc.route$().url.pathname === '/' && Object.keys(loc.route$().url.searchParams).length === 0
       return isAtRoot ? loc.back() : loc.replaceState({}, '', '/')
-    } else return loc.back()
+    } else {
+      return loc.back()
+    }
   })
 }
